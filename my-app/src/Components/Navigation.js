ab0c8e461db3dcd6b35383b98ff7dda21a9e4924
Michal Walczak <michalwalczak777@gmail.com>
2020-12-03 02:42:09 +0100

    app has been styled

--- my-app/src/Components/Navigation.js
+++ my-app/src/Components/Navigation.js
@@ -7,12 +7,15 @@ const Pokemon = () => {
     
     return (
         <nav className="navigation-nav">
-            <ul className="navigation-list">
-                <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/about"}>About</Link></li>
-                <li><Link to={"/pokemons"}>Pokemons</Link></li>
-                <li><Link to={"/favourites"}>Favourites</Link></li>
-            </ul>
+            <div className="container navigation-container">
+                <h2>POKEWEB</h2>
+                <ul className="navigation-list">
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/about"}>About</Link></li>
+                    <li><Link to={"/pokemons"}>Pokemons</Link></li>
+                    <li><Link to={"/favourites"}>Favourites</Link></li>
+                </ul>
+            </div>
         </nav>
     )
 }
